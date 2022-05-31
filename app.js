3b0643fc81f4c52c83b85b62c8f905c2e4031f65
Montana <montanacoding@outlook.com>
2022-06-01 02:54:32 +1000

    commit on 'main'

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ window.onload = () => {}
     let questionArray = [
         `${solicitation} word that you love`,
         `${solicitation} year ("2002", "2011" etc)`,
+        `${solicitation} type of animal`,
+        `${solicitation} verb (past tense)`,
+        `${solicitation} pronoun`,
+        `${solicitation} noun`,
+        `${solicitation}n adjective`,
+        `${solicitation} number`,
+        `${solicitation} word with 4 letters`,
+        `${solicitation} type of tool`,
+        `${solicitation} verb`,
+        `${solicitation} number`,
+        `${solicitation} name of a woman`,
+        `${solicitation} title of a movie`,
+        `${solicitation} noun`,
+        `${solicitation} verb`,
+        `${solicitation} type of job`,
+        `${solicitation} double digit number`,
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+        `${solicitation}`
+
         
     ]
 
